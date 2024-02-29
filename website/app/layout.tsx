b567cff1afc3f824e81fd012b6802008f82d7039
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2024-02-29 21:58:04 +0100

    website setup

---
+++ website/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sudoku Solver",
+  description: "Sudoku Solver website made by Cogito NTNU",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
