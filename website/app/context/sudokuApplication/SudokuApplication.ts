16a206687c283e05c3e4ea81551901dbb6f22eeb
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2024-04-16 22:48:49 +0200

    fix(website): typo

--- website/app/context/sudokuApplication/SudokuApplication.ts
+++ website/app/context/sudokuApplication/SudokuApplication.ts
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react"
-import { SudokuApplication } from "./types"
+import { SudokuApplication } from "./Types"
 
 
 export const SudokuApplicationContext = createContext<SudokuApplication | null>(null)
