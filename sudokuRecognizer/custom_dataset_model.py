02453e51487ef513dfd983e591996a598a8b2956
Afras Mansoor <afras@hotmail.no>
2024-04-29 11:44:58 +0200

    fix custom data file

--- sudokuRecognizer/custom_dataset_model.py
+++ sudokuRecognizer/custom_dataset_model.py
@@ -1,52 +1,113 @@
 import numpy as np
-from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.optimizers import Adam
+from keras.losses import CategoricalCrossentropy
+from keras.metrics import Accuracy
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn.preprocessing import LabelBinarizer
+import tensorflowjs as tfjs
 
 label_path = "website/prisma/seeding/labels.bin"
 digits_path = "website/prisma/seeding/digits.bin"
+model_path = "new_model"
 
-# Read labels and digits
-labels = np.fromfile(label_path, dtype=np.uint8)
-digit_buffer = np.fromfile(digits_path, dtype=np.uint8)
-num_images = len(labels)
-digits = digit_buffer.reshape((num_images, 28, 28))
 
-# Remove all zeroes from custom dataset
-non_zero_mask = labels != 0
-labels = labels[non_zero_mask]
-digits = digits[non_zero_mask]
+def split_data(x: np.ndarray, y: np.ndarray) -> tuple[tuple[np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray]]:
+    i = int(len(y) * 0.8)
+    return (x[:i], y[:i]), (x[i:], y[i:])
 
-# Load MNIST dataset
-(x_train_mnist, y_train_mnist), (_, _) = mnist.load_data()
 
-# Filter out zeroes from MNIST dataset
-non_zero_mask_mnist = y_train_mnist != 0
-x_train_mnist = x_train_mnist[non_zero_mask_mnist]
-y_train_mnist = y_train_mnist[non_zero_mask_mnist]
+# Read labels
+labels: np.ndarray
+with open(label_path, "rb") as f:
+    labels = np.frombuffer(f.read(), dtype=np.uint8)
 
-# Calculate number of MNIST images to use
-num_mnist_images = int(len(labels) * 0.3)  # 30% of the number of custom dataset images
+# Read digits
+digits: np.ndarray
+size = 28*28
+with open(digits_path, "rb") as f:
+    digit_buffer = np.frombuffer(f.read(), dtype=np.uint8)
 
-# Randomly select MNIST images
-mnist_indices = np.random.choice(len(y_train_mnist), num_mnist_images, replace=False)
-x_train_mnist_selected = x_train_mnist[mnist_indices]
-y_train_mnist_selected = y_train_mnist[mnist_indices]
+    num_images = len(labels)
+    digits = np.empty((num_images, 28, 28))
 
-# Concatenate custom dataset with selected MNIST images
-x_train = np.concatenate((digits, x_train_mnist_selected))
-y_train = np.concatenate((labels, y_train_mnist_selected))
+    for n in range(num_images):
+        img = digit_buffer[n*size:(n+1)*size]
+        img = img.reshape((28, 28))
+        digits[n] = img
 
-# Shuffle dataset
-shuffle_indices = np.random.permutation(len(y_train))
-x_train = x_train[shuffle_indices]
-y_train = y_train[shuffle_indices]
 
-# Turn labels into vectors
+
+# for i in range(10):
+#     print(i, len(labels[np.where(labels == i)]))
+
+# Remove all zeroes
+zero_mask = np.where(labels != 0)
+labels = labels[zero_mask]
+digits = digits[zero_mask]
+
+
+# Split into train and test
+(x_train, y_train), (x_test, y_test) = split_data(digits, labels)
+
+x_train = x_train.astype(np.float32) / 255.0
+x_test = x_test.astype(np.float32) / 255.0
+
+
+# # Turn labels into vectors
 le = LabelBinarizer()
 y_train = le.fit_transform(y_train)
+y_test = le.transform(y_test)
 
-# Normalize
-x_train = x_train / 255.0
+x_train = np.expand_dims(x_train, axis=-1)  # Add an extra dimension for the channels
+x_test = np.expand_dims(x_test, axis=-1)
 
-print("x_train shape:", x_train.shape)
-print("y_train shape:", y_train.shape)
+datagen = ImageDataGenerator(
+            featurewise_center=False,  # set input mean to 0 over the dataset
+            samplewise_center=False,  # set each sample mean to 0
+            featurewise_std_normalization=False,  # divide inputs by std of the dataset
+            samplewise_std_normalization=False,  # divide each input by its std
+            zca_whitening=False,  # apply ZCA whitening
+            # rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+            # zoom_range = 0.1, # Randomly zoom image 
+            width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+            height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+            horizontal_flip=False,  # randomly flip images
+            vertical_flip=False)  # randomly flip images
+
+# Define model
+model = Sequential()
+    
+IMAGE_WIDTH = 28
+IMAGE_HEIGHT = 28
+IMAGE_CHANNELS = 1
+
+model.add(Conv2D(filters=8, kernel_size=5, strides=1, activation='relu', 
+                    kernel_initializer='glorot_uniform', 
+                    input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Conv2D(filters=16, kernel_size=5, strides=1, activation='relu', 
+                    kernel_initializer='glorot_uniform'))
+
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dense(units=9, activation='softmax',
+                kernel_initializer='glorot_uniform'))
+
+model.compile(optimizer=Adam(),
+                loss=CategoricalCrossentropy(),
+                metrics=[Accuracy()])
+
+model.compile(optimizer="adam", loss="categorical_crossentropy",
+              metrics=["accuracy"])
+
+model.fit(x_train, y_train, validation_data=(x_train, y_train),
+          batch_size=64, epochs=8, shuffle=True, verbose=1)
+
+
+tfjs.converters.save_keras_model(model, model_path)
