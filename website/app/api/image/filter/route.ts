7c77cc65feb6aa182ec3d581983905312a30abf2
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2024-05-04 17:15:38 +0200

    fix: remove data collecting pages

--- website/app/api/image/filter/route.ts
+++
@@ -1,7 +0,0 @@
-// import prisma from "@/prisma/prisma"
-
-export async function POST(request: Request) {
-    const data = request.body
-    console.log(data)
-    return new Response(JSON.stringify({response: "yes"}))
-}
